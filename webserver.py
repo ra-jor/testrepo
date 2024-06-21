67248fc88e9b0e456d52834ea0f47d905bcf069d
ra-jor <jordan.cheong@redalphacyber.com>
2024-06-21 02:00:02 -0400

    Cleanup

--- webserver.py
+++ webserver.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-if __name__ == '__main__':
+
+def main():
     app.run(host='0.0.0.0', port=80, debug=True)
+
+
+if __name__ == '__main__':
+    main()
+
